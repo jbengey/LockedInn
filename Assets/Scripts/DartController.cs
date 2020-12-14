a6c8cf4a2ff18f1d8b1e42a84544940ed33d8494
JBengey <jack.bengeyfield@gmail.com>
2020-12-14 23:03:21 +0000

    Added models for dartboard and dart Added dartboardController script and logic for score Added basic regidbodyfreeze

---
+++ Assets/Scripts/DartController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartController : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //If colliding with anything that is not a dart
+        if (!collision.gameObject.CompareTag("Dart"))
+        {
+            Rigidbody dartRB = this.gameObject.GetComponent<Rigidbody>(); //reference to the dart rigidbody
+            dartRB.constraints = RigidbodyConstraints.FreezeAll; //freeze dart - getting it stuck in whatever it has hit
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/DartboardController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DartboardController : MonoBehaviour
+{
+    public Transform centre;
+    public int playerScore = 0;
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        //Check that the entered collider is a dart
+        if (other.gameObject.CompareTag("Dart"))
+        {
+            Vector3 collisionPoint = other.contacts[0].point; //reference to the exact point of the collider that hit the dartboard
+            playerScore += CalculateScore(collisionPoint);
+        }
+    }
+
+
+    private int CalculateScore(Vector3 CollisionPoint)
+    {
+        int dartMultiplier;
+        int calculatedScore;
+        float dartDistance, dartDistanceX, dartDistanceY;
+        double dartAngle;
+
+        dartDistance = Vector2.Distance(new Vector2(centre.position.x,centre.position.y), new Vector2(CollisionPoint.x, CollisionPoint.y) );    //Calculate distance between dart and the bullseye (centre)
+        dartDistanceX = CollisionPoint.x - centre.position.x ; //x direction of dart
+        dartDistanceY = CollisionPoint.y - centre.position.y ; //Y direction of dart
+        dartAngle = Math.Atan2(dartDistanceY,dartDistanceX); //Trig to caluclate angle of dart relative to centre, in radians
+        dartAngle = dartAngle * 180 / Math.PI; //convert radians to degrees
+
+        //Check for bull 50 and 25
+        if (dartDistance < 0.00034f)
+        {
+            calculatedScore = 50; //Bullseye!
+        }
+        else if (dartDistance > 0.00033f && dartDistance < 0.00066f)
+        {
+            calculatedScore = 25; //Bull!
+        }
+        else
+        {
+            //// Detection based on distance (Score multiplier)
+            //if (55.7f > dartDistance && dartDistance < 61.4f)
+            //{
+            //    //Inner triple ring
+            //    dartMultiplier = 3;
+            //}
+            //else if (92.2f > dartDistance && dartDistance < 98.0f)
+            //{
+            //    //Outer double ring
+            //    dartMultiplier = 2;
+            //}
+            //else
+            //{
+            //    //Normal area - single scores
+                dartMultiplier = 1;
+            //}
+            //Debug.Log(dartMultiplier);
+
+
+            //Detection based on angle (Hit score number)
+            int hitNumber = dartAngle < 9 ? 6
+                          : dartAngle > 9 && dartAngle < 27 ? 13
+                          : dartAngle > 27 && dartAngle < 45 ? 4
+                          : dartAngle > 45 && dartAngle < 63 ? 18
+                          : dartAngle > 63 && dartAngle < 81 ? 1
+                          : dartAngle > 81 && dartAngle < 99 ? 20
+                          : dartAngle > 99 && dartAngle < 117 ? 5
+                          : dartAngle > 117 && dartAngle < 135 ? 12
+                          : dartAngle > 135 && dartAngle < 153 ? 9
+                          : dartAngle > 153 && dartAngle < 171 ? 14
+                          : dartAngle > 171 && dartAngle < 189 ? 11
+                          : dartAngle > 189 && dartAngle < 207 ? 8
+                          : dartAngle > 207 && dartAngle < 225 ? 16
+                          : dartAngle > 225 && dartAngle < 243 ? 7
+                          : dartAngle > 243 && dartAngle < 261 ? 19
+                          : dartAngle > 261 && dartAngle < 279 ? 3
+                          : dartAngle > 279 && dartAngle < 297 ? 17
+                          : dartAngle > 297 && dartAngle < 315 ? 2
+                          : dartAngle > 315 && dartAngle < 333 ? 15
+                          : dartAngle > 333 && dartAngle < 351 ? 10
+                          : dartAngle > 351 && dartAngle < 360 ? 6
+                          : 0;
+
+            calculatedScore = hitNumber * dartMultiplier;
+        }
+
+        return calculatedScore;
+    }
+
+
+}
