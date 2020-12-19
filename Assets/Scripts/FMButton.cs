1a66edb74e73f538bb94a8b7f316eea22518ebdd
2030428 <72548933+2030428@users.noreply.github.com>
2020-12-19 16:46:38 +0000

    minor adjustments to fruit machine button - commit for merging

--- Assets/Scripts/FMButton.cs
+++ Assets/Scripts/FMButton.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class FMButton : MonoBehaviour
 {
+    public GameObject button;
     public bool buttonPressed;
+    public Material on, off;
 
     private void Start()
     {
+        button.GetComponent<MeshRenderer>().material = off;
         buttonPressed = false;
+    }
+
+    private void Update()
+    {
+        if (buttonPressed)
+        {
+            button.GetComponent<MeshRenderer>().material = on;
+        }
+        else
+        {
+            button.GetComponent<MeshRenderer>().material = off;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
