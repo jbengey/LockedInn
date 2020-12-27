510a7f7216bad7f53332d4a26fe3a287bad29016
jbengey <72210823+jbengey@users.noreply.github.com>
2020-12-27 23:17:17 +0000

    - Added Custom slot machine object to main - Added destrcution handler script - Added broken glass beer mug prefab, with script attached - Added glass shatter sound effect

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject originalModel,destroyedObject; //ref to the seperate gameobject version
+    public float timeToDespawn = 0.0f;
+    public float velocityNeededToBreak = 0.5f;
+
+    GameObject instanciated;
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (destroyedObject == null) Debug.LogError("Destructible scirpt has no destoryed object provided", this);
+        if (destroyedObject != null && collision.relativeVelocity.magnitude > velocityNeededToBreak)
+        {
+            //Hide - needs to be hidden so script still runs
+            Destroy(originalModel);
+
+            //Instanciate the destroyed model
+            instanciated = Instantiate(destroyedObject, transform.position, transform.rotation);
+
+            foreach (Transform child in instanciated.transform)
+            {
+                //add force relative to the speed of the collison to each rigidbody, at center of the collision
+                child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(5 * collision.relativeVelocity.magnitude, collision.contacts[0].point,1.5f);
+            }
+
+            //Coroutine to remove shattered object after specified time
+            StartCoroutine(DestroyAfterTime(instanciated, timeToDespawn));
+        }
+    }
+
+
+    private IEnumerator DestroyAfterTime(GameObject myobject, float time)
+    {
+        if (time != 0)
+        {
+            yield return new WaitForSeconds(time);
+            Destroy(myobject); //remove instanciated broken object
+        }
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/MeshHighlighter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshHighlighter : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Get material 
+    }
+
+
+
+}
