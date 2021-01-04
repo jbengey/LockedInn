79cac4872d03a43f577491b6946f8fe8831f0941
jbengey <72210823+jbengey@users.noreply.github.com>
2021-01-04 22:05:59 +0000

    - Changed roof transparency draw priority

--- Assets/Scripts/XRPlayerCollisionManager.cs
+++ Assets/Scripts/XRPlayerCollisionManager.cs
@@ -17,13 +17,8 @@ public class XRPlayerCollisionManager : MonoBehaviour
         if(characterController.center.x != vRCamera.localPosition.x | characterController.center.z != vRCamera.localPosition.z)
         {
             //Align character controller to the camera - avoids issues if player moves inside VR playzone
-            characterController.center = new Vector3(vRCamera.localPosition.x, 0.84f, vRCamera.localPosition.z);
+            characterController.center = new Vector3(vRCamera.localPosition.x, 0.86f, vRCamera.localPosition.z);
         }
 
     }
-
-
-
-
-
 }
